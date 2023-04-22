05c287ff5fffd543a5e85cfd51493ddcf877c02e
mampionona33 <ramamps33@gmail.com>
2023-04-22 14:35:53 +0300

    finish to setup entry point of project

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once "./controllers/UserController.php";
 $uri = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 
-if ($uri == "/" || $uri == "/index.php" || $uri == "/list.php") {
-    show_list();
+if ($uri == "/" || $uri == "/index.php" || $uri == "/list") {
+    echo "test";
+    // show_list();
 }
