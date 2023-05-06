2c8f2752386b5477147960c2d4b4bee49477ead2
Mampionona33 <ramamps33@gmail.com>
2023-05-06 19:47:29 +0300

    finish self hosting material icons

--- template/template.php
+++ template/template.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="ut">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../dist/style.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" rel="stylesheet">
     <title><?php echo $title; ?></title>
 </head>
 
