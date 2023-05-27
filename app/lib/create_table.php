ba6482e878681a0b780cc5120541921e60725052
Mampionona <50689325+Mampionona33@users.noreply.github.com>
2023-05-27 06:04:10 +0000

    strat change some fileé

--- app/lib/create_table.php
+++ app/lib/create_table.php
@@ -1,7 +1,69 @@
 <?php
+/* 
+    exemple usage :
+    $columns = [
+    [
+        'name' => 'id',
+        'type' => 'INT',
+        'required' => true,
+        'auto_increment' => true,
+    ],
+    [
+        'name' => 'name',
+        'type' => 'VARCHAR(255)',
+        'required' => true,
+        'auto_increment' => false,
+    ],
+    [
+        'name' => 'email',
+        'type' => 'VARCHAR(255)',
+        'required' => false,
+        'auto_increment' => false,
+    ],
+];
+create_table('users', $columns);
+*/
+
+require_once "./connect_db.php";
+
+require_once "./connect_db.php";
+
 function create_table($nom_table, $col)
 {
-    $db = connect_db();
-    $sql = "CREATE TABLE IF NOT EXISTS $nom_table($col);";
-    $db->exec($sql);
+    try {
+        $db = connect_db();
+
+        $columns = [];
+        $primaryKeys = [];
+
+        foreach ($col as $column) {
+            $columnName = $column['name'];
+            $columnType = $column['type'];
+            $isRequired = $column['required'] ? 'NOT NULL' : '';
+            $isAutoIncrement = $column['auto_increment'] ? 'AUTO_INCREMENT' : '';
+
+            if ($columnType === 'ENUM') {
+                $enumValues = "'" . implode("', '", $column['values']) . "'";
+                $defaultValue = "'" . $column['values'][0] . "'";
+                $columns[] = "$columnName $columnType($enumValues) $isRequired $isAutoIncrement DEFAULT $defaultValue";
+            } else {
+                $columns[] = "$columnName $columnType $isRequired $isAutoIncrement";
+            }
+
+            if ($column['auto_increment']) {
+                $primaryKeys[] = $columnName;
+            }
+        }
+
+        $columnsString = implode(', ', $columns);
+        $primaryKeysString = implode(', ', $primaryKeys);
+
+        $sql = "CREATE TABLE IF NOT EXISTS $nom_table ($columnsString, PRIMARY KEY ($primaryKeysString));";
+        $db->exec($sql);
+
+        // echo "Table créée avec succès !";
+        return true;
+    } catch (PDOException $e) {
+        echo "Erreur lors de la création de la table : " . $e->getMessage();
+    }
 }
--- app/lib/get_data.php
+++ app/lib/get_data.php
@@ -3,20 +3,35 @@
 /* 
 Exemple d'utilisation :
 ----------------------
-$table_name = "users";
-$query = "WHERE age > 25 ORDER BY nom ASC";
-$data = get_data($table_name, $query); 
+
 *************************************
 *->fetchAll(PDO::FETCH_ASSOC) : C'est une méthode de l'objet PDOStatement qui récupère tous les enregistrements de résultat de la requête dans un tableau.
 
 *PDO::FETCH_ASSOC : C'est une constante définie par PDO qui spécifie le mode de récupération des données. Dans ce cas, FETCH_ASSOC signifie que chaque enregistrement est retourné sous la forme d'un tableau associatif, où les noms des colonnes de la table sont utilisés comme clés.
 */
 
-function get_data($nom_table, $query)
+function get_data($nom_table, $list_colonne = [], $condition = "")
 {
     $db = connect_db();
-    $sql = "SELECT * FROM $nom_table $query";
-    $stmt = $db->prepare($sql);
+    $query = "SELECT";
+
+    // Si la liste de colonnes est vide, sélectionner toutes les colonnes avec *
+    if (empty($list_colonne)) {
+        $query .= "*";
+    } else {
+        // Sinon, ajouter les colonnes à la requête
+        $query .= implode(", ", $list_colonne);
+    }
+
+    // Ajouter la table à la requête
+    $query .= " FROM " . $nom_table;
+
+    // Si une condition est spécifiée, l'ajouter à la requête
+    if (!empty($condition)) {
+        $query .= " WHERE " . $condition;
+    }
+
+    $stmt = $db->prepare($query);
 
     try {
         $stmt->execute();
